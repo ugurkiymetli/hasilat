96bfc453ca66bd3ca3c662d25a91a65e0d00c121
ugurkiymetli <ugurkiymetli@hotmail.com>
2024-09-26 13:58:38 +0300

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import React from "react";
 import LoginForm from "@/components/Auth/LoginForm";
 import GoldPrices from "@/components/GoldPrices";
 import { useAdmin } from "@/contexts/AdminContext";
+import Footer from "@/components/Layout/Footer";
+import Header from "@/components/Layout/Header";
 
 export default function Home() {
   const { isAdmin, login, logout } = useAdmin();
 
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
-      <header>
-        <h1 className="text-4xl font-bold mb-8 text-center">Hasılat</h1>
-      </header>
+      <Header title="Hasılat" />
       <main className="flex-grow flex items-center justify-center p-8">
         <div className="w-full max-w-md">
           {!isAdmin ? (
@@ -22,16 +22,7 @@ export default function Home() {
           )}
         </div>
       </main>
-      <footer className="py-4 text-center bg-gray-900">
-        <a
-          href="https://github.com/your-github-username/your-repo-name"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-400 hover:text-blue-300 transition duration-150 ease-in-out"
-        >
-          View on GitHub
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
